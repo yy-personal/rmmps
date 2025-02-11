b2b11645eeb8c7f18f6c4b43a700c1a6948451ef
Lil-YH <73232658+Lil-YH@users.noreply.github.com>
2025-02-11 22:04:15 +0800

    backend spring setup

---
+++ src/main/java/com/nus_iss/recipe_management/RecipeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nus_iss.recipe_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagementApplication.class, args);
+	}
+
+}
