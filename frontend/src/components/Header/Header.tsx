7f62f7ba49cc2760b6e05a27a2f8c02997b04ea3
Muqaffa <purifish97@gmail.com>
2025-02-16 10:29:30 +0800

    fix: fix rerouting to homepage upon clicking app name in the header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -54,7 +54,7 @@ function Header() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
